61ac702ac1a2131d9ec029133fdfaec59ce2164e
tothdomi22 <tothdomi22@gmail.com>
2024-07-31 19:24:59 +0200

    feat: registration

--- src/main/java/com/dominik/controller/AuthController.java
+++ src/main/java/com/dominik/controller/AuthController.java
@@ -4,9 +4,10 @@ import com.dominik.entity.User;
 import com.dominik.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class AuthController {
@@ -16,9 +17,17 @@ public class AuthController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+//    expects a json
+//    @PostMapping("/register")
+//    public User createUser(@RequestBody User user) {
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        return userRepository.save(user);
+//    }
+
     @PostMapping("/register")
-    public User createUser(@RequestBody User user) {
+    public RedirectView createUser(@ModelAttribute User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+        userRepository.save(user);
+        return new RedirectView("/index.html");  // Redirect after successful registration
     }
 }
