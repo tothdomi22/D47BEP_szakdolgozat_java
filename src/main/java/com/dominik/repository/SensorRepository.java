31236b841aac8b85cfcb7655006e4941f5366f35
dominiktoth <tothdomi22@gmail.com>
2024-11-03 11:32:43 +0100

    feat: code cleanup

--- src/main/java/com/dominik/repository/SensorRepository.java
+++ src/main/java/com/dominik/repository/SensorRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface SensorRepository extends JpaRepository<Sensor, Integer>{
 
     Sensor findFirstByOrderByIdDesc();
+
     List<Sensor> findTop42ByOrderByIdDesc();
 }
--- src/main/java/com/dominik/repository/UserRepository.java
+++ src/main/java/com/dominik/repository/UserRepository.java
@@ -7,10 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByUsername(String username);
 
     @Query("select u.username, u.id, u.role  from User u")
     List<String> getAllUserData();
-
-
 }
